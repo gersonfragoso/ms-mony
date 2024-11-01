73c6f3b64da22bec525de42bc05f5187a267332e
vpfernanda <fernandavpagano@gmail.com>
2024-11-01 10:30:24 -0300

    DTOS: created. (Email, Payment -> write and read DTOS.)

---
+++ payment/src/main/java/com/mony/payment/model/dtos/PaymentReadDTO.java
@@ -0,0 +1,24 @@
+package com.mony.payment.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentReadDTO {
+    private UUID paymentId;
+    private String orderNumber;
+    private BigDecimal value;
+    private String cpf;
+    private String number;
+    private LocalDateTime paymentDate;
+    private UUID userId;
+    private String nameUser;
+    private String email;
+}
