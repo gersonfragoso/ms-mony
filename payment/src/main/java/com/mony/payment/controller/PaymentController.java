fa3c3523ac117387719dc05ced6bfccdbf17b9be
vpfernanda <fernandavpagano@gmail.com>
2024-11-10 12:16:11 -0300

    Swagger configuration.

--- payment/src/main/java/com/mony/payment/controller/PaymentController.java
+++ payment/src/main/java/com/mony/payment/controller/PaymentController.java
@@ -12,8 +12,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 import java.util.UUID;
 
+@OpenAPIDefinition(info = @Info(title = "Mony - Payment API", version = "1.0", description = "API para o processamento de pagamentos"))
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
@@ -28,6 +37,13 @@ public class PaymentController {
     /*URL PARA TESTE: http://localhost:8081/payments/id
     ATENÇÃO: necessário colocar header: KEY: id; value: seu UUID.
     * */
+    @Operation(summary = "Obter pagamento por ID [HEADER]", description = "Recupera o pagamento utilizando o" +
+            " ID fornecido via header.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pagamento encontrado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Pagamento não encontrado."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @GetMapping("/id")
     public ResponseEntity<PaymentReadDTO> getPaymentById(@RequestHeader UUID id) {
         try {
@@ -45,6 +61,13 @@ public class PaymentController {
     /*Exemplo de URL para a requisição:
     http://localhost:8081/payments/id/UUID-AQUI
     * */
+    @Operation(summary = "Obter pagamento por ID [URL PATH]", description = "Recupera o pagamento utilizando" +
+            " o ID fornecido diretamente na URL.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pagamento encontrado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Pagamento não encontrado."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @GetMapping("/id/{id}")
     public ResponseEntity<PaymentReadDTO> getPaymentByIdInPath(@PathVariable UUID id) {
         try {
@@ -60,74 +83,71 @@ public class PaymentController {
     /*URL para testar:
     http://localhost:8081/payments/cpf/SEUCPFAQUI?page=0&size=10
     * */
+    @Operation(summary = "Obter pagamentos por CPF [URL PATH]", description = "Recupera os pagamentos associados" +
+            " a um CPF específico fornecido via URL.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pagamentos do CPF encontrados com sucesso."),
+            @ApiResponse(responseCode = "204", description = "Sem conteúdo - nenhum pagamento encontrado para o CPF informado."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @GetMapping("/cpf/{cpf}")
     public ResponseEntity<Page<PaymentReadDTO>> getPaymentsByCpfInPath(@PathVariable String cpf, Pageable pageable) {
         try {
             Page<PaymentReadDTO> payments = paymentService.getPaymentsByCpf(cpf, pageable);
 
             if (payments.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); // 204 No Content
             }
-            return ResponseEntity.status(HttpStatus.OK).body(payments);
+            return ResponseEntity.status(HttpStatus.OK).body(payments); // 200 OK
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // 500 Internal Server Error
         }
     }
 
-
     /*exemplo de URL para o get abaixo:
-    * http://localhost:8080/payments?page=0&size=10
-    * Necessário ajustar no postman um Header: KEY: cpf , VALUE: o número do cpf pesquisado.
-    * */
-
+     * http://localhost:8080/payments?page=0&size=10
+     * Necessário ajustar no postman um Header: KEY: cpf , VALUE: o número do cpf pesquisado.
+     * */
+    @Operation(summary = "Obter pagamentos por CPF [HEADER]", description = "Recupera os pagamentos associados" +
+            " ao CPF informado no header da requisição.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pagamentos do CPF encontrados com sucesso."),
+            @ApiResponse(responseCode = "204", description = "Sem conteúdo - nenhum pagamento encontrado para o CPF."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @GetMapping
     public ResponseEntity<Page<PaymentReadDTO>> getPaymentsByCpf(@RequestHeader String cpf, Pageable pageable) {
-        try{
+        try {
             Page<PaymentReadDTO> payments = paymentService.getPaymentsByCpf(cpf, pageable);
 
             if (payments.isEmpty()) {
-                // 204 No Content -> se não há pagamentos para o CPF fornecido
-                return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+                return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); // 204 No Content
             }
-            return ResponseEntity.status(HttpStatus.OK).body(payments);
-        }
-        catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.OK).body(payments); // 200 OK
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // 500 Internal Server Error
         }
     }
 
-    /*
-    JSON PARA CRIAÇÃO POST
-                {
-              "orderCode": "123456789",
-              "value": 150.75,
-              "cpf": "12345678901",
-              "nameCard": "João Silva",
-              "numberCard": "1234567890123456",
-              "dueDate": "12/25",
-              "code": "123",
-              "userId": "550e8400-e29b-41d4-a716-446655440000",
-              "nameUser": "Maria Oliveira",
-              "email": "maria.oliveira@example.com"
-            }
-
-     */
+    @Operation(summary = "Processar pagamento", description = "Processa um pagamento com as" +
+            " informações fornecidas, requisitando API externa e gravando no banco de dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Pagamento criado com sucesso. Obs: não necessariamente a operação" +
+                    " de pagamento foi confirmada."),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor.")
+    })
     @PostMapping
     public ResponseEntity<PaymentReadDTO> processPayment(@RequestBody @Valid PaymentWriteDTO payment) {
         try {
             PaymentReadDTO processedPayment = paymentService.processPayment(payment);
-            // Retorna 201 Created -> pagamento processado com sucesso
-            return ResponseEntity.status(HttpStatus.CREATED).body(processedPayment);
+            return ResponseEntity.status(HttpStatus.CREATED).body(processedPayment); // 201 Created
         } catch (IllegalArgumentException e) {
-            // Retorna 400 Bad Request -> dados inválidos
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(null);
+                    .body(null); // 400 Bad Request
         } catch (Exception e) {
-            // Retorna 500 Internal Server Error -> qualquer outra exceção inesperada
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(null);
+                    .body(null); // 500 Internal Server Error
         }
     }
-
-
 }
